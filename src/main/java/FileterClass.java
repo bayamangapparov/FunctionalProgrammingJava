83d2c81cc96ac51e8d8475180ec6cb780b4b13b5
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-02-20 14:43:30 -0600

    Filter example

---
+++ src/main/java/FileterClass.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FileterClass {
+
+	public static void main(String[] args) {
+		
+		List<String> names = Arrays.asList("Baiaman", "Emil", "Elbek", "Ruslan", "Joomar", "Bainazar", "Ulan", "Ernazar");
+
+		names.stream()
+//			.filter(new Predicate<String>() {
+//				public boolean test(String name) {
+//					return name.equals("Baiaman");
+//				}
+//			})
+			.filter(name -> !name.equals("Baiaman"))
+			.forEach(name -> System.out.println(name));
+	}
+
+}
